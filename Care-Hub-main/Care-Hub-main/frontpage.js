8c36759a73101092011cef4752ac8d2179bab934
Dhaboomya <126372080+Dhaboomya@users.noreply.github.com>
2024-03-11 14:48:41 +0530

    Add files via upload

---
+++ Care-Hub-main/Care-Hub-main/frontpage.js
@@ -0,0 +1,15 @@
+function like() {
+	alert("You liked this post!");
+}
+
+function comment() {
+	alert("You commented on this post!");
+}
+
+function repost() {
+	alert("You reposted this post!");
+}
+
+function send() {
+	alert("You sent this post to a friend!");
+}
